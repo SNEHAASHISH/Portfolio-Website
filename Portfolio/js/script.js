c6c14a5f89b8d2ba63680cd8e8384ef1e4cb9225
SNEHAASHISH <85332537+SNEHAASHISH@users.noreply.github.com>
2021-06-16 22:14:53 +0530

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Sophomore @ MNIT Jaipur","Java and Spring Enthusiast", "Eat, Sleep, Code... Repeat!"],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+    	}
+	});
+
+
+    
+
+
+	var skillsTopOffSet = $(".skillsSection").offset().top;
+	$(window).scroll(function() {
+
+		if (window.pageYOffset > skillsTopOffSet - $(window).height() + 200) {
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+    		});
+		}
+
+	});
+
+});
